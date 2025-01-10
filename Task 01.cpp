bdf99f8dfd05874bca2b17566e8db6af1c8a2ecc
Syed Shaheer Ahsan <shaheerahsan4@gmail.com>
2025-01-11 00:14:28 +0500

    Update Task 01.cpp

--- Task 01.cpp
+++ Task 01.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main() {
 
-    int arr[4][4];
+    int arr2d[4][4];
     cout<<"Enter the values to store in Array:"<<endl;
     for(int i=0; i<4; i++)
     {
         for(int j=0; j<4; j++)
         {
-            cin>>arr[i][j];
+            cin>>arr2d[i][j];
         }
     }
     cout<<endl;
@@ -17,7 +17,7 @@ int main() {
     {
         for(int l=0; l<4; l++)
         {
-            cout<<arr[k][l]<<" , ";
+            cout<<arr2d[k][l]<<" , ";
         }
         cout<<endl;
     }
